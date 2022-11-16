21b3e09949659abde8b28694df05a61967e0175e
Folke Lemaitre <folke.lemaitre@gmail.com>
2022-11-16 10:31:04 +0100

    build: formatting

--- lua/paint/config.lua
+++ lua/paint/config.lua
@@ -13,26 +13,26 @@ M.ns = vim.api.nvim_create_namespace("paint.nvim")
 
 --- @class PaintOptions
 M.defaults = {
-	---@type PaintHighlight[]
-	highlights = {
-		{
-			-- filter can be a table of buffer options that should match,
-			-- or a function called with buf as param that should return true.
-			-- The example below will paint @something in comments with Constant
-			filter = { filetype = "lua" },
-			pattern = "%s*%-%-%-%s*(@%w+)",
-			hl = "Constant",
-		},
-	},
+  ---@type PaintHighlight[]
+  highlights = {
+    {
+      -- filter can be a table of buffer options that should match,
+      -- or a function called with buf as param that should return true.
+      -- The example below will paint @something in comments with Constant
+      filter = { filetype = "lua" },
+      pattern = "%s*%-%-%-%s*(@%w+)",
+      hl = "Constant",
+    },
+  },
 }
 
 --- @type PaintOptions
 M.options = {}
 
 function M.setup(options)
-	options = options or {}
+  options = options or {}
 
-	M.options = vim.tbl_deep_extend("force", {}, M.defaults, options)
+  M.options = vim.tbl_deep_extend("force", {}, M.defaults, options)
 end
 
 return M
--- lua/paint/init.lua
+++ lua/paint/init.lua
@@ -2,8 +2,8 @@ local M = {}
 
 ---@param opts? PaintOptions
 function M.setup(opts)
-	require("paint.config").setup(opts)
-	require("paint.highlight").enable()
+  require("paint.config").setup(opts)
+  require("paint.highlight").enable()
 end
 
 return M
